d92df25964c3f06517d9277297564199bac457e0
Alex <mail@alexkam.ru>
2022-01-24 07:14:28 +0300

    2 zadanie

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,13 @@
 const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 const NODE_ENV = process.env.NODE_ENV;
+const IS_DEV = NODE_ENV == 'development';
 
 module.exports = {
+    resolve:{
+        extensions:['.js','.jsx','.ts','.tsx','.json']
+    },
     mode: NODE_ENV ? NODE_ENV : 'development',
     entry: path.resolve(__dirname,'src/index.jsx'),
     output: {
@@ -14,5 +19,13 @@ module.exports = {
             test: /\.[tj]sx?$/,
             use: ['ts-loader']
         }]
+    },
+    plugins:[
+        new HTMLWebpackPlugin({template: path.resolve(__dirname,'index.html')})
+    ],
+    devServer:{
+        port: 3000,
+        open: true,
+        hot: IS_DEV
     }
 };
